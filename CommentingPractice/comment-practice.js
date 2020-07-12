536cabd9c9e7abe9ff79b4b9f137cc58f2f21d11
Henry Chen <henrychen256@gmail.com>
2020-07-12 15:02:16 -0400

    had issues with node.js'

--- CommentingPractice/comment-practice.js
+++ CommentingPractice/comment-practice.js
@@ -107,8 +107,8 @@ import * as tree from './model/plant.js';
     member.hello();
 
     //Utilize plant.js for task 3 ~ Henry Chen
-    const oakTree1= new OakTree("Herbert", 60, false, "Chestnut Oak");
-    console.log(`${oakTree1.name} is a ${oakTree1.type()}`);
+    const oakTree1= new tree.OakTree("Herbert", 60, false, "Chestnut Oak");
+    console.log(`${oakTree1.name} is a ${oakTree1.type()}.`);
 
 
  })();
